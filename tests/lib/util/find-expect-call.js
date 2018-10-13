8192f60eda2aeed6eb8c0adb3f53d92b24191e33
Kenton Jacobsen <kenton.jacobsen@gmail.com>
2018-10-13 05:41:01 -0400

    Fix expect finder utility (#26)
    * Demonstrate error in find-expect-call only finding particular expects
    
    * Better recurse for different definitions of expects

---
+++ tests/lib/util/find-expect-call.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var expect = require('chai').expect;
+var espree = require('espree');
+var findExpectCall = require('../../../lib/util/find-expect-call');
+
+describe('find-expect-call util', function () {
+  it('Finds expect statements which are considered member expressions', function () {
+    var code = 'expect(true).to.be.ok;';
+    var ast = espree.parse(code);
+    var result = findExpectCall(ast.body[0].expression);
+    expect(result).to.be.an('object');
+  });
+
+  it('Finds expect statements which are considered recursive call expressions', function () {
+    var code = 'expect(true).to.equal(true);';
+    var ast = espree.parse(code);
+    var result = findExpectCall(ast.body[0].expression);
+    expect(result).to.be.an('object');
+  });
+});
