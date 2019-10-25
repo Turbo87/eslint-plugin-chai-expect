24587ba3274aad47bbcd7f160a9225ac0bad85fe
Brett Zamir <brettz9@yahoo.com>
2019-10-25 17:59:37 +0800

    - Add inner literal rule; fixes #17

---
+++ tests/lib/rules/no-inner-literal.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const rule = require('../../../lib/rules/no-inner-literal');
+const {RuleTester} = require('eslint');
+
+let ruleTester = new RuleTester();
+ruleTester.run('no-inner-literal', rule, {
+  valid: [{
+    code: 'expect(a).to.be.ok;'
+  }, {
+    code: 'expect(a && b).to.be.ok;'
+  }, {
+    code: 'expect(a || b).to.be.ok;'
+  }, {
+    code: 'expect(a).to.equal(5);'
+  }, {
+    code: 'expect(`template literal`).to.equal(5);',
+    parserOptions: {
+      ecmaVersion: 2015
+    }
+  }, {
+    code: 'expect(tagged`template literal`).to.equal(5);',
+    parserOptions: {
+      ecmaVersion: 2015
+    }
+  }],
+
+  invalid: [{
+    code: 'expect(undefined).to.be.ok;',
+    errors: [{
+      message: '`undefined` used in expect()'
+    }]
+  }, {
+    code: 'expect(null).to.be.ok;',
+    errors: [{
+      message: '`null` used in expect()'
+    }]
+  }, {
+    code: 'expect(true).to.be.ok;',
+    errors: [{
+      message: '`true` used in expect()'
+    }]
+  }, {
+    code: 'expect(52).to.be.ok;',
+    errors: [{
+      message: '`52` used in expect()'
+    }]
+  }, {
+    code: 'expect("string").to.be.ok;',
+    errors: [{
+      message: '`"string"` used in expect()'
+    }]
+  }, {
+    code: 'expect(132n).to.be.ok;',
+    errors: [{
+      message: '`132n` used in expect()'
+    }],
+    parserOptions: {
+      ecmaVersion: 2020
+    }
+  }, {
+    code: 'expect(/regex/).to.be.ok;',
+    errors: [{
+      message: '`/regex/` used in expect()'
+    }]
+  }, {
+    code: 'expect(NaN).to.be.ok;',
+    errors: [{
+      message: '`NaN` used in expect()'
+    }]
+  }, {
+    code: 'expect(Infinity).to.be.ok;',
+    errors: [{
+      message: '`Infinity` used in expect()'
+    }]
+  }, {
+    code: 'expect(+Infinity).to.be.ok;',
+    errors: [{
+      message: '`+Infinity` used in expect()'
+    }]
+  }, {
+    code: 'expect(-Infinity).to.be.ok;',
+    errors: [{
+      message: '`-Infinity` used in expect()'
+    }]
+  }, {
+    code: 'expect(this).to.be.ok;',
+    errors: [{
+      message: '`this` used in expect()'
+    }]
+  }, {
+    code: 'expect(false).to.equal(true);',
+    errors: [{
+      message: '`false` used in expect()'
+    }]
+  }]
+});
