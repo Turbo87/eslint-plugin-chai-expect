eaf4b7c9f6483220c1224f51d6b77541ef97f9d9
Tobias Bieniek <tobias@bieniek.cloud>
2024-06-13 09:21:37 +0200

    tests: Make `ecmaVersion()` compatible with ESLint 9

--- tests/lib/rules/utils.js
+++ tests/lib/rules/utils.js
@@ -1,4 +1,16 @@
+const eslintPkg = require('eslint/package.json');
+
+const USE_LANGUAGE_OPTIONS = eslintPkg.version.startsWith('9.');
+
 function ecmaVersion(version) {
+  if (USE_LANGUAGE_OPTIONS) {
+    return {
+      languageOptions: {
+        ecmaVersion: version
+      }
+    };
+  }
+
   return {
     parserOptions: {
       ecmaVersion: version
